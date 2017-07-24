96fdf32fdf58c2a359c99f90be4d1f192b49ba9e
van hong xuan <xuanvan229@gmail.com>
2017-07-24 22:12:32 +0700

    learn go folder

---
+++ GOLearn/index.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+
+}
+func main() {
+
+	fmt.Println("hello world!")
+	f := fib()
+	fmt.Println(f(), f(), f(), f(), f())
+	var pi float64 = 3.14159265
+	fmt.Printf("%.3f ", pi)
+}
